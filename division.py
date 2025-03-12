508561bfc493503d38a2ab149abb56773875ef93
hasan8303 <mhzaidi59@gmail.com>
2025-03-12 20:15:42 +0530

    division program added

---
+++ division.py
@@ -0,0 +1,9 @@
+a = int(input("enter the number"))
+
+b = a//3
+c = a /3
+d = a % 3
+
+print(b)
+print(c)
+print(d)
---
+++ even_or_odd.py
@@ -0,0 +1,8 @@
+# write a program to check if the number is even odd
+
+a = int(input("enter the number"))
+
+if a%2==0:
+    print("the number is even")
+else:
+    print("the number is odd")
